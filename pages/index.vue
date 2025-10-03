0aecd2c5f4f7a392fa1d731dd150fa65e6b93e29
mdhamine <143548635+mdhamine@users.noreply.github.com>
2025-10-03 16:31:54 +0100

    v1

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,45 @@
 import { MainTraining } from '#build/components'; import { MainNextlvl } from
 '#build/components'; import Mission from '~/components/Main/mission.vue';
 <template>
-	<main>
-		<MainNavbar />
-		<MainHero />
-		<LazyMainMission />
-		<LazyMainNextlvl />
+  <main>
+    <MainNavbar />
+    <MainHero />
+    <LazyMainMission />
+    <LazyMainNextlvl />
 
-		<LazyMainTraining />
-		<LazyMainFeat />
-		<LazyMainFAQs />
-		<LazyMainFooter />
-	</main>
+    <LazyMainTraining />
+    <LazyMainFeat />
+    <LazyMainFAQs />
+    <LazyMainNewsection />
+    <LazyMainFooter />
+  </main>
 </template>
 
 <style>
 * {
-	transition: all 0.25s ease-out;
-	-webkit-overflow-scrolling: touch;
+  transition: all 0.25s ease-out;
+  -webkit-overflow-scrolling: touch;
 }
 
 ::-webkit-scrollbar {
-	width: 15px;
+  width: 15px;
 }
 
 ::-webkit-scrollbar-track {
-	background-color: rgba(0, 0, 0, 0.05);
-	-webkit-border-radius: 20px;
-	border-radius: 20px;
+  background-color: rgba(0, 0, 0, 0.05);
+  -webkit-border-radius: 20px;
+  border-radius: 20px;
 }
 
 ::-webkit-scrollbar-thumb {
-	-webkit-border-radius: 20px;
-	border-radius: 20px;
-	background: rgb(54, 49, 49);
+  -webkit-border-radius: 20px;
+  border-radius: 20px;
+  background: rgb(54, 49, 49);
 }
 </style>
 
 <script setup lang="ts">
 useHead({
-	title: 'Home',
+  title: "Home",
 });
 </script>
